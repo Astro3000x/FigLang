897581f4729d227b382c5d5b7d2112fe87ce47df
Astro3000 <pwill7131@gmail.com>
2022-06-02 01:32:03 +0000

    Readme updates, added String variables and comments.

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 ##
-fig
+Fig
 ##
+
+![](figlogo.png)
+
+
+To Use Fig, clone this repo and write your code in main.fig
+
+`out-"Hello World"` Print Command
+
+`in-"What's your name?"` Take input and return the input
+
+`str-wasp = "seed";` Declare a string variable.
+
 a fig is a fruit (wow bet you didn't know that, huh?)
--- main.py
+++ main.py
@@ -1,26 +1,44 @@
 f = open("main.fig", "r")
 fig = str(f.read())
-
+variables = {}
 while 1:
-  if "out-'" in fig:
-    f1 = fig.split("out-'")
-    f2 = f1[1].split("'")
-    fig = fig.replace("out-'"+f2[0]+"';", '')
-    print(f2[0])
-  elif 'out-"' in fig:
-    f1 = fig.split('out-"')
-    f2 = f1[1].split('"')
-    fig =  fig.replace('out-"'+f2[0]+'";', '')
-    print(f2[0])
-  elif "in-'" in fig:
-    f1 = fig.split("in-'")
+  
+ if "str-" in fig:
+   f1 = fig.replace("str-", "")
+   f2 = f1.split(" = ")
+   varname = f2[0]
+   f3 = f2[1].split(";")
+   f4 = f3[0]
+   f5 = f4.replace("'", "")
+   varvalue = f5.replace('"', '')
+   variables[varname] = varvalue
+   
+   fig = fig.replace(f"str-{varname} = {f4};", "")
+ if "/-" in fig or '/-' in fig:
+    f1 = fig.replace("/-", "")
+    
+    
+    
+    
+    fig = fig.replace("/-"+f1, "")
+ if "out-'" in fig or 'out-"' in fig:
+   f1 = fig.replace("out-", "")
+   f2 = f1.replace("'", "")
+   f3 = f2.replace('"', '')
+   f4 = f3.split(";")
+   print(f4[0])
+   fig = fig.replace("out-"+f1, "")
+  
+ if "in-'" in fig:
+    f1 = fig.split("in '")
     f2 = f1[1].split("'")
     fig = fig.replace("in-'"+f2[0]+"';", '')
     inp = input(f2[0])
     print(inp)
-  elif "stop;" in fig:
-    print("\nExit Status 1")
+ if "stop;" in fig:
+    print("Exit Status 1")
+    
     break
-  else:
-    print("\nExit Status 0")
+ else:
+    print("Exit Status 0")
     break
